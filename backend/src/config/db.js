ea257ba5a9987d4729d6aead873766273e299c16
rajeevkmr <rajeev.kumar@intelliswift.com>
2025-04-22 06:49:01 +0530

    fixed issue related to menu

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,6 @@
 import { mongoose } from 'mongoose';
 const connectDB = async () => {
   try {
-    console.log('process.env', process.env.MONGODB_URI);
     await mongoose
       .connect(process.env.MONGODB_URI)
       .then(() => {
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Header from './components/Header/header.component';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JavaScript
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
